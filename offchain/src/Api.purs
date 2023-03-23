f809bb6f7285a1e3d79ea7b96deed39472114765
zmrocze <karolochmanmilarski@gmail.com>
2023-03-23 14:42:34 +0100

    Serve frontend with webpack - no css(?)

--- offchain/src/Api.purs
+++ offchain/src/Api.purs
@@ -1,16 +1,18 @@
 module MlabsPlutusTemplate.Api
   (
     square
-  , module Export
+  -- , module Export
   )
   where
 
 import Prelude
 import Data.Function.Uncurried (Fn1, mkFn1)
-import MLabsPlutusTemplate.Scripts (always_succeeds) as Export
+-- import 
+-- import MLabsPlutusTemplate.Scripts (always_succeeds) as Export
 -- import Contract.JsSdk
 --   ( runContractJS
 --   )
+-- import Data.Int (pow)
 
 square :: Fn1 Int Int
-square = mkFn1 $ \n -> n*n
+square = mkFn1 $ \n -> n * n
