8ab8ed95997e9f6ba59506db7e64e6a13d30e239
zmrocze <karolochmanmilarski@gmail.com>
2023-03-23 21:01:27 +0100

    Add node-polyfill-webpack-plugin
    It provides some API that I have no clue if our code needs. But ctl uses it so likely some ctl code needs it.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,8 @@
 
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
 
@@ -15,6 +17,16 @@ module.exports = {
       directory: path.join(__dirname, "build"),
     },
     port: 4008,
+    // Dont know if we need this - let's uncomment if some error message contains word "kupo"
+    // proxy: {
+    //   "/kupo": {
+    //     // `KUPO_HOST` env variable must be set to the base URL of the Kupo
+    //     // service, otherwise all requests to Kupo will fail.
+    //     target: process.env.KUPO_HOST || "http://localhost:1442",
+    //     changeOrigin: true,
+    //     pathRewrite: { "^/kupo": "" },
+    //   },
+    // },
   },
 
   devtool: 'inline-source-map',
@@ -43,9 +55,29 @@ module.exports = {
 
   resolve: {
     extensions: [".js", ".jsx", ".tsx", ".ts"],
+    fallback: {
+      buffer: require.resolve("buffer/"),
+      http: false,
+      url: false,
+      stream: false,
+      crypto: false,
+      https: false,
+      net: false,
+      tls: false,
+      zlib: false,
+      os: false,
+      path: false,
+      fs: false,
+      readline: false,
+      child_process: false,
+    },
   },
 
   plugins: [
+    new NodePolyfillPlugin(),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
     new HtmlWebpackPlugin({
       title: "mlabs-plutus-scaffold",
       template: path.join(__dirname, "public", "index.html"),
